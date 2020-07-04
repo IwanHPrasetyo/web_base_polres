c974b15bef1e0121fc244f9a59e471cba776b537
Iwan H Prasetyo <55027286+IwanHPrasetyo@users.noreply.github.com>
2020-07-05 02:31:14 +0700

    redesign

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Menu from "../components/Menu";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+
+export default Dashboard;
